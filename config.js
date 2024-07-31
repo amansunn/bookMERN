8a6e1c64b86f0061b1e745a514ea781950a29e5b
amansunn <lukamatic4ever@gmail.com>
2024-07-31 16:26:43 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+export const PORT = 5555;
+
+
+
+// export const password = encodeURIComponent('root');
+
+
+export const mongoDBURL = 'mongodb+srv://root:root@books-store-mern.iazd11j.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
+
+
